f842f65184752d71ae0662113e33a7094e2ed67e
yasasvi <yasasvigumma@gmail.com>
2023-12-31 22:09:57 +0530

    Learning to use bun:sqlite

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import { Database } from "bun:sqlite";
 
-const db = new Database("./bun.db");
+const db = new Database("base.db", { create: true });
 
-//db.query("create table test(id integer primary key, name varchar(25));");
+let query = db.query(
+  `create table foo(id int primary key,username varchar(225),passwd varchar(225));`
+);
+query.run();
+
+query = db.query(`insert into foo values(1,'admin','1234');`);
+query.run();
+
+query = db.query(`select * from foo`);
+console.log(query.values());
+
+db.close();
